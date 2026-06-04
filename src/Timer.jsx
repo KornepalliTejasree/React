4ffcdfb84c22ba6dd5da568eafd599dc0a87cbcd
KornepalliTejasree <tejasree7718@gmail.com>
2026-06-04 19:33:27 +0530

    Timer modified

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -3,9 +3,14 @@ import React, { useEffect, useState } from 'react'
 const Timer = () => {
     const [time,setTime]=useState(0);
     useEffect(()=>{
-        setInterval(()=>{
-        setTime((prev)=>prev=prev+1);
+        console.log("Timer started");
+       const interval= setInterval(()=>{
+        setTime((prev)=>prev+1);
         },1000);
+        return ()=>{
+         console.log("Timer stopped");
+         clearInterval(interval);
+        }
     },[]);
   return (
     <div>
