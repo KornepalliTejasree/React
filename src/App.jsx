6c92bbb5ee323705474314f5befcb18fdf33d960
KornepalliTejasree <tejasree7718@gmail.com>
2026-06-14 18:20:21 +0530

    API fetching studied

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
-import Dashboard from './components/Dashboard';
-import {UserContext} from "./components/UserContext"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import "./App.css"
 const App = () => {
-  const [name,setName]=useState("Rohith");
-  const [count,setCount]=useState(100);
+  
+  const [products,setProducts]=useState([]);
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products").then((res)=>setProducts(res.data)).
+    catch((err)=>{console.log("Error fetching")})
+  },[])
+ 
   return (
-    <UserContext.Provider value={{name,count}}>
-      <p>App</p>
-      <Dashboard/>
-    </UserContext.Provider>
+   <div className='products-container'>
+   <h2 className='products-title'>Products-List</h2>
+   <div className='products-grid'>
+    {
+      products.map((product)=>(
+        <div className='product-card' key={product.id}>
+          <img src={product.image} alt="" />
+          <h3>{product.title}</h3>
+          <p>{product.price}</p>
+          <p>{product.rating.rate}</p>
+        </div>
+      ))
+    }
+   </div>
+   </div>
   )
 }
 
--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useContext } from 'react'
 import {UserContext} from "./UserContext"
 const Username = () => {
-  const {name,count}=useContext(UserContext);
+  const {name,count,increase}=useContext(UserContext);
   return (
     <div>
       <p>Username:{name}</p>
       <p>Count:{count}</p>
+      <button onClick={increase}>Increase</button>
     </div>
   )
 }
